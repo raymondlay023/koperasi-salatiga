a0ed59366795bc3b82818d41d54435a767e54cea
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-07-03 08:49:49 +0700

    usercontroller add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
 
 class UserController extends Controller
 {
-    //
+    public function indexowner()
+    {
+        $users = User::where('role_id',2)->get();
+        $roles = Role::get()->get();
+        return view();
+    }
 }
